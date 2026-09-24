8707474ad18ad1266e8af3d848c44646aa495a33
Yusuf <contact@yisawi.me>
2026-09-24 05:14:23 +0300

    chore(db): add NOT NULL constraints to student_id and device_sn in attendance_logs

---
+++ db/migrations/000016_harden_attendance_logs.down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE attendance_logs ALTER COLUMN student_id DROP NOT NULL;
+ALTER TABLE attendance_logs ALTER COLUMN device_sn DROP NOT NULL;
---
+++ db/migrations/000016_harden_attendance_logs.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE attendance_logs ALTER COLUMN student_id SET NOT NULL;
+ALTER TABLE attendance_logs ALTER COLUMN device_sn SET NOT NULL;
